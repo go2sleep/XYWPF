a916ce1c119bdbe4889b1a3ee393aa89df0efc4b
go2sleep <yuanling.gu>
2020-02-25 16:36:05 +0800

    增加控件样式模板Demo

--- XYWPF.Sample/MainWindow.xaml.cs
+++ XYWPF.Sample/MainWindow.xaml.cs
@@ -29,34 +29,33 @@ namespace XYWPF.Sample
 
         private void LoadBackgroundImages()
         {
-            Dictionary<string, string> dictSkin = new Dictionary<string, string>();
-            dictSkin.Add("Generic", "pack://application:,,,/XYWPF.Skin;component/Images/Generic/MainWindowBackground.jpg");
-            dictSkin.Add("Theme1", "pack://application:,,,/XYWPF.Skin;component/Images/Theme1/MainWindowBackground.jpg");
+            Dictionary<string, Brush> dictSkin = new Dictionary<string, Brush>();
+            dictSkin.Add("Generic", new SolidColorBrush(Color.FromRgb(62, 140, 206)));
+            dictSkin.Add("Red", new SolidColorBrush(Color.FromRgb(218, 83, 80)));
+            dictSkin.Add("Green", new SolidColorBrush(Color.FromRgb(90, 185, 93)));
 
             foreach (var skin in dictSkin)
             {
-                Image imgTheme = new Image()
+                Border Boder = new Border()
                 {
                     Width = 100,
-                    Stretch = Stretch.Fill,
                     Height = 60,
+                    Background = skin.Value,
                     Margin = new Thickness(2)
                 };
 
-                imgTheme.Name = System.IO.Path.GetFileNameWithoutExtension(skin.Value);
-                imgTheme.Tag = skin.Key;
-                imgTheme.Source = new BitmapImage(new Uri(skin.Value, UriKind.Absolute));
-                imgTheme.MouseLeftButtonDown += new MouseButtonEventHandler(imgTheme_MouseLeftButtonDown);
-                ImagePanel.Children.Add(imgTheme);
+                Boder.Tag = skin.Key;
+                Boder.MouseLeftButtonDown += new MouseButtonEventHandler(Theme_MouseLeftButtonDown);
+                ImagePanel.Children.Add(Boder);
             }
         }
 
-        private void imgTheme_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        private void Theme_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            Image image = sender as Image;
-            if (image != null && image.Source != null)
+            FrameworkElement eleSkin = sender as FrameworkElement;
+            if (eleSkin != null && eleSkin.Tag != null)
             {
-                var skinDictUri = new Uri($"pack://application:,,,/XYWPF.Skin;component/Themes/{image.Tag.ToString()}.xaml", UriKind.Absolute);                
+                var skinDictUri = new Uri($"pack://application:,,,/XYWPF.Skin;component/Themes/{eleSkin.Tag.ToString()}.xaml", UriKind.Absolute);                
 
                 ResourceDictionary skinResource = new ResourceDictionary();
                 skinResource.Source = skinDictUri;
